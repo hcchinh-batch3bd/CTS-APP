d0194a1cac0d4505e14f03ea2f45c0f0155d29fb
hcchinh-batch3bd <hcchinh-batch3bd@webmail.edu.vn>
2020-03-18 14:07:22 +0700

    Add project files.

---
+++ CTS-beta/frmLogin.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CTS_beta
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.materialTheme1 = new Telerik.WinControls.Themes.MaterialTheme();
+            this.txtID = new Telerik.WinControls.UI.RadTextBox();
+            this.txtPassword = new Telerik.WinControls.UI.RadTextBox();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.fluentDarkTheme1 = new Telerik.WinControls.Themes.FluentDarkTheme();
+            this.radCheckBox1 = new Telerik.WinControls.UI.RadCheckBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.txtID)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radCheckBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.BackColor = System.Drawing.Color.Transparent;
+            this.txtID.ForeColor = System.Drawing.Color.Black;
+            this.txtID.Location = new System.Drawing.Point(41, 130);
+            this.txtID.Name = "txtID";
+            this.txtID.NullText = "Mã đăng nhập";
+            this.txtID.ShowClearButton = true;
+            this.txtID.ShowNullText = true;
+            this.txtID.Size = new System.Drawing.Size(259, 36);
+            this.txtID.TabIndex = 0;
+            this.txtID.ThemeName = "Material";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.AcceptsReturn = true;
+            this.txtPassword.ForeColor = System.Drawing.Color.Black;
+            this.txtPassword.Location = new System.Drawing.Point(41, 203);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.NullText = "Mật khẩu";
+            this.txtPassword.PasswordChar = '●';
+            this.txtPassword.ShowClearButton = true;
+            this.txtPassword.ShowNullText = true;
+            this.txtPassword.Size = new System.Drawing.Size(259, 36);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.ThemeName = "Material";
+            // 
+            // radButton1
+            // 
+            this.radButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(92)))), ((int)(((byte)(250)))));
+            this.radButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radButton1.ForeColor = System.Drawing.Color.White;
+            this.radButton1.Image = global::CTS_beta.Properties.Resources.login_32px;
+            this.radButton1.Location = new System.Drawing.Point(165, 291);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(135, 36);
+            this.radButton1.TabIndex = 3;
+            this.radButton1.Text = "Đăng nhập";
+            this.radButton1.TextAlignment = System.Drawing.ContentAlignment.MiddleRight;
+            this.radButton1.ThemeName = "Material";
+            // 
+            // radCheckBox1
+            // 
+            this.radCheckBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.radCheckBox1.Location = new System.Drawing.Point(107, 256);
+            this.radCheckBox1.Name = "radCheckBox1";
+            this.radCheckBox1.Size = new System.Drawing.Size(125, 18);
+            this.radCheckBox1.TabIndex = 0;
+            this.radCheckBox1.Tag = "";
+            this.radCheckBox1.Text = "Ghi nhớ đăng nhập ?";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World);
+            this.linkLabel1.ForeColor = System.Drawing.Color.Blue;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(222, 429);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(101, 15);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Quên mật khẩu ?";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(335, 453);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.radCheckBox1);
+            this.Controls.Add(this.radButton1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtID);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmLogin";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "ĐĂNG NHẬP";
+            this.ThemeName = "Material";
+            ((System.ComponentModel.ISupportInitialize)(this.txtID)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radCheckBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.Themes.MaterialTheme materialTheme1;
+        private Telerik.WinControls.UI.RadTextBox txtID;
+        private Telerik.WinControls.UI.RadTextBox txtPassword;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.Themes.FluentDarkTheme fluentDarkTheme1;
+        private Telerik.WinControls.UI.RadCheckBox radCheckBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
