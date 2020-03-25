6ff3995bd83befc93a68387d419e4f1053aa69ed
Nguyễn Dương Minh Quang <minhquangcoder@users.noreply.github.com>
2020-03-25 12:51:37 +0700

    Revert "fix"
    This reverts commit 0bcd917bca6414617268739e2aff13205dd737ac.

--- CTS-beta/Form_CTS/frmLogin.Designer.cs
+++ CTS-beta/Form_CTS/frmLogin.Designer.cs
@@ -32,15 +32,15 @@
             this.materialTheme1 = new Telerik.WinControls.Themes.MaterialTheme();
             this.txtID = new Telerik.WinControls.UI.RadTextBox();
             this.txtPassword = new Telerik.WinControls.UI.RadTextBox();
-            this.radCheckBox1 = new Telerik.WinControls.UI.RadCheckBox();
+            this.ckbRemember = new Telerik.WinControls.UI.RadCheckBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.btnLogin = new Telerik.WinControls.UI.RadButton();
             ((System.ComponentModel.ISupportInitialize)(this.txtID)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPassword)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radCheckBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ckbRemember)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLogin)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
@@ -73,16 +73,17 @@
             this.txtPassword.TabIndex = 1;
             this.txtPassword.ThemeName = "Material";
             // 
-            // radCheckBox1
+            // ckbRemember
             // 
-            this.radCheckBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.radCheckBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.radCheckBox1.Location = new System.Drawing.Point(105, 320);
-            this.radCheckBox1.Name = "radCheckBox1";
-            this.radCheckBox1.Size = new System.Drawing.Size(125, 18);
-            this.radCheckBox1.TabIndex = 2;
-            this.radCheckBox1.Tag = "";
-            this.radCheckBox1.Text = "Ghi nhớ đăng nhập ?";
+            this.ckbRemember.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ckbRemember.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.ckbRemember.Location = new System.Drawing.Point(105, 320);
+            this.ckbRemember.Name = "ckbRemember";
+            this.ckbRemember.Size = new System.Drawing.Size(119, 18);
+            this.ckbRemember.TabIndex = 2;
+            this.ckbRemember.Tag = "";
+            this.ckbRemember.Text = "Tự động đăng nhập";
+            this.ckbRemember.CheckStateChanging += new Telerik.WinControls.UI.CheckStateChangingEventHandler(this.ckbRemember_CheckStateChanging);
             // 
             // linkLabel1
             // 
@@ -112,20 +113,21 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // radButton1
+            // btnLogin
             // 
-            this.radButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.radButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(92)))), ((int)(((byte)(250)))));
-            this.radButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.radButton1.ForeColor = System.Drawing.Color.White;
-            this.radButton1.Image = global::CTS_beta.Properties.Resources.login_32px;
-            this.radButton1.Location = new System.Drawing.Point(163, 355);
-            this.radButton1.Name = "radButton1";
-            this.radButton1.Size = new System.Drawing.Size(135, 36);
-            this.radButton1.TabIndex = 3;
-            this.radButton1.Text = "Đăng nhập";
-            this.radButton1.TextAlignment = System.Drawing.ContentAlignment.MiddleRight;
-            this.radButton1.ThemeName = "Material";
+            this.btnLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(92)))), ((int)(((byte)(250)))));
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Image = global::CTS_beta.Properties.Resources.login_32px;
+            this.btnLogin.Location = new System.Drawing.Point(163, 355);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(135, 36);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.TextAlignment = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLogin.ThemeName = "Material";
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // frmLogin
             // 
@@ -136,8 +138,8 @@
             this.ClientSize = new System.Drawing.Size(339, 527);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.radCheckBox1);
-            this.Controls.Add(this.radButton1);
+            this.Controls.Add(this.ckbRemember);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtID);
             this.ForeColor = System.Drawing.Color.Transparent;
@@ -150,12 +152,13 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ĐĂNG NHẬP";
             this.ThemeName = "Material";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmLogin_FormClosing);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmLogin_MouseDown);
             ((System.ComponentModel.ISupportInitialize)(this.txtID)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.txtPassword)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radCheckBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ckbRemember)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLogin)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -167,8 +170,8 @@
         private Telerik.WinControls.Themes.MaterialTheme materialTheme1;
         private Telerik.WinControls.UI.RadTextBox txtID;
         private Telerik.WinControls.UI.RadTextBox txtPassword;
-        private Telerik.WinControls.UI.RadButton radButton1;
-        private Telerik.WinControls.UI.RadCheckBox radCheckBox1;
+        private Telerik.WinControls.UI.RadButton btnLogin;
+        private Telerik.WinControls.UI.RadCheckBox ckbRemember;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
